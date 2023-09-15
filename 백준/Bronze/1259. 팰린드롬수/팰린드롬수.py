804f6470994bcd12ac47379d133c426d6ef619d8
Vinca0121 <98202797+Vinca0121@users.noreply.github.com>
2023-09-15 14:17:31 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,12 +1,8 @@
-def is_palindrome(num_str):
-    reversed_str = num_str[::-1]  # 문자열을 뒤집음
-    return num_str == reversed_str
-
 while True:
-    num_str = input()
-    if num_str == '0':
+    number = input()
+    if number[0] == '0':
         break
-    if is_palindrome(num_str):
-        print('yes')
-    else:
-        print('no')
+    if number[::-1] == number :
+        print("yes")
+    else :
+        print("no")
