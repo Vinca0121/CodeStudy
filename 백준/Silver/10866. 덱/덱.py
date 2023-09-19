a10983afd324d79504deb10f4c5ebfa6ebeee596
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-09-19 10:17:52 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -4,30 +4,29 @@ input = sys.stdin.readline
 t = int(input())
 
 
-my_deque = []
+my_deque = deque()
 
 for i in range(t):
     str = input().split()
     case = str[0]
     if case == 'push_front':
-        my_deque = [str[1]] + my_deque
+        my_deque.appendleft(str[1])
 
     elif case == 'push_back':
-        my_deque = my_deque + [str[1]]
+        my_deque.append(str[1])
 
     elif case == 'pop_front':
         if(len(my_deque) == 0):
             print(-1)
         else :
-            print(my_deque[0])
-            del my_deque[0]
+            print(my_deque.popleft())
+
 
     elif case == 'pop_back':
         if(len(my_deque) == 0):
             print(-1)
         else :
-            print(my_deque[-1])
-            del my_deque[-1]
+            print(my_deque.pop())
 
     elif case == 'front':
         if(len(my_deque) == 0):
