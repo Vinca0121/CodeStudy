a292a3cda6008d58620bd00e3e621b9bfaf9d445
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-09-19 14:27:20 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,29 +1,19 @@
 import sys
 input = sys.stdin.readline
+
 n, k = map(int, input().split())
 
+# 찾고자하는 인덱스
+idx = 0
+
+# 리스트 생성
 n_list = [i+1 for i in range(n)]
+# len이 0이상일때 반복
+print("<", end="")
+while len(n_list) > 1 :
+    # 인덱스를 계산
+    idx = (idx + k -1) % len(n_list)
+    print(n_list[idx], end=", ")
+    del n_list[idx]
+print(n_list[-1], end=">")
 
-ans = []
-while len(n_list) >= k:
-    # k번째 값 찾기
-    ans.append(n_list[k-1])
-    # k번째 값 이후 부터를 앞으로 보냄
-    n_list = n_list[k:] + n_list[:k-1]
-    # 맨 뒤에꺼 삭제    del n_list[-1]
-
-for i in range(len(n_list)):
-    p = k % len(n_list)
-    if p>0:
-        ans.append(n_list[p-1])
-        n_list = n_list[p:] + n_list[:p-1]
-    else :
-        ans.append(n_list[-1])
-        n_list = n_list[:-1]
-
-    # 뒤에 남은 거 다 추가 (수정필요)
-print('<', end="")
-for i in range(len(ans) -1):
-    print(ans[i], end=", ")
-print(ans[-1], end="")
-print('>', end=" ")
