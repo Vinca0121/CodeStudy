aa4a57426562a475febfd2661f9c2997126369cf
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-09-22 11:08:20 +0900

    [Silver IV] Title: 듣보잡, Time: 4588 ms, Memory: 37400 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,10 +1,17 @@
-a, b = map(int, input().split())
+a_set = set()
+ans_list = []
 
-# a_list를 set으로 변경하여 데이터 검색을 빠르게 수행할 수 있도록 합니다.
-a_set = set(input().rstrip() for _ in range(a))
+a ,b = map(int, input().split())
 
-result = sorted(name for name in (input().rstrip() for _ in range(b)) if name in a_set)
+for _ in range(a):
+    a_set.add(input().strip())
 
-print(len(result))  # 찾은 이름의 총 수 출력
-for name in result:
-    print(name)  # 찾은 이름 출력
+for _ in range(b):
+    name = input().strip()
+    if name in a_set:
+        ans_list.append(name)
+
+ans_list.sort()
+print(len(ans_list))
+for n in ans_list:
+    print(n)
