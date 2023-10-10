18510efbf0692d2b6f76935250142e985a6e3a53
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-10-10 21:47:23 +0900

    [Silver II] Title: 나무 자르기, Time: 4500 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -5,21 +5,28 @@ n, m = map(int, input().split())
 trees = list(map(int, input().rstrip().split()))
 trees.sort()
 
-left = 0
-right = max(trees)
-result = 0
 
+left = 0
+right = trees[-1]
+max_tall = 0
 while left <= right:
     mid = (left + right) // 2
-    total = 0
-    for tree in trees:
-        if tree > mid:
-            total += tree - mid
-    
-    if total >= m:
-        result = mid
+    cutting_sum = 0
+
+    for t in trees:
+        if t > mid :
+            cutting_sum += (t - mid)
+
+    # 벌목하는 범위를 더 타이트하게 만들었을 때도 정답이 나오는가를 테스트
+    if cutting_sum > m:
+        max_tall = mid
         left = mid + 1
-    else:
+
+    elif cutting_sum < m:
         right = mid - 1
 
-print(result)
+    else:
+        max_tall = mid
+        break
+    
+print(max_tall)
