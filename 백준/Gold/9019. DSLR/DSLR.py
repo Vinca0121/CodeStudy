514b38dbab506737b0e09e2d7fdb4dd51276f6d2
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-10-17 11:56:26 +0900

    [Gold IV] Title: DSLR, Time: 8900 ms, Memory: 213748 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -1,29 +1,32 @@
 from collections import deque
 
 def bfs(end):
-    visited = [False] * 10000
-    my_que = deque([('D', start, '')])
-    visited[start] = True
 
     while my_que:
-        oper, current, path = my_que.popleft()
+        start, ans = my_que.popleft()
 
-        if current == end:
-            return path
+        if start == end:
+            return ans
 
-        d = (current * 2) % 10000
-        s = current - 1 if current != 0 else 9999
-        l = (current % 1000) * 10 + current // 1000
-        r = (current % 10) * 1000 + current // 10
+        d = (start * 2) % 10000
+        s = start - 1 if start != 0 else 9999
+        l = (start % 1000) * 10 + start // 1000
+        r = (start % 10) * 1000 + start // 10
 
         for next_oper, next_state in [('D', d), ('S', s), ('L', l), ('R', r)]:
             if not visited[next_state]:
                 visited[next_state] = True
-                my_que.append((next_oper, next_state, path + next_oper))
+                my_que.append((next_state, ans + next_oper))
 
 t = int(input().rstrip())
+# 큐 정의
+my_que = deque()
 
 for _ in range(t):
     start, end = map(int, input().rstrip().split())
+    visited = [False] * 10000
+    my_que.append((start, ''))
+    visited[start] = True
     result = bfs(end)
     print(result)
+    my_que.clear()
