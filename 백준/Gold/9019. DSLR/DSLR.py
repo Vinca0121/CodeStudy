a929e11f849cc84483f9188366c3a27d58631617
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-10-17 12:22:26 +0900

    [Gold IV] Title: DSLR, Time: 6184 ms, Memory: 213748 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def bfs(end):
+def bfs(end, visited):
 
     while my_que:
         start, ans = my_que.popleft()
@@ -8,15 +8,30 @@ def bfs(end):
         if start == end:
             return ans
 
-        d = (start * 2) % 10000
-        s = start - 1 if start != 0 else 9999
-        l = (start % 1000) * 10 + start // 1000
-        r = (start % 10) * 1000 + start // 10
+        # 첫 연산에서 4개의 연산의 경우를 모두 실행 (BFS)
+        # 각 연산을 취했을 때의 값을 계산
+        d_num = (start * 2) % 10000
+        s_num = start - 1 if start != 0 else 9999
+        l_num = (start % 1000) * 10 + start // 1000
+        r_num = (start % 10) * 1000 + start // 10
 
-        for next_oper, next_state in [('D', d), ('S', s), ('L', l), ('R', r)]:
-            if not visited[next_state]:
-                visited[next_state] = True
-                my_que.append((next_state, ans + next_oper))
+        # 각 연산의 값을 큐에 추가
+        if not visited[d_num]:
+            visited[d_num] = True
+            my_que.append((d_num, ans + 'D'))
+
+        if not visited[s_num]:
+            visited[s_num] = True
+            my_que.append((s_num, ans + 'S'))
+
+        if not visited[l_num]:
+            visited[l_num] = True
+            my_que.append((l_num, ans + 'L'))
+
+        if not visited[r_num]:
+            visited[r_num] = True
+            my_que.append((r_num, ans + 'R'))
+
 
 t = int(input().rstrip())
 # 큐 정의
@@ -25,8 +40,7 @@ my_que = deque()
 for _ in range(t):
     start, end = map(int, input().rstrip().split())
     visited = [False] * 10000
-    my_que.append((start, ''))
     visited[start] = True
-    result = bfs(end)
-    print(result)
+    my_que.append((start, ''))
+    print(bfs(end, visited))
     my_que.clear()
