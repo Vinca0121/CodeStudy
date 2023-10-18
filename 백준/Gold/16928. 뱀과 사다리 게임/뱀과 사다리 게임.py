b34bec0cb34ac98f5db708f8a07c1f7c83a76b05
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-10-18 15:54:01 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34204 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,34 +1,48 @@
 from collections import deque
+ladder, snake = map(int, input().split())
 
-def bfs(my_map):
-    visited = [False] * 101  # 방문 여부를 저장할 리스트
-    visited[1] = True
-    queue = deque()
-    queue.append((1, 0))  # 시작 위치와 주사위 횟수
+# 전체 범위
+my_map = [0] * 101
+# 순간 이동 위치를 지정
+for _ in range(ladder):
+    start, end = map(int, input().split())
+    my_map[start] = end
 
+for _ in range(snake):
+    start, end = map(int, input().split())
+    my_map[start] = end
+
+# 방문 여부 표시
+visited = [False] * 101
+queue = deque()
+# 시작 위치 (0,0)
+queue.append((1,0))
+visited[1] = True
+
+def bfs():
     while queue:
         current_position, roll = queue.popleft()
-        # 주사위 눈(1부터 6)에 대해 이동을 탐색
+        # 1~6 주사위에 대한 값에 대해서 수행
         for dice in range(1, 7):
             new_position = current_position + dice
             # 목적지(100)에 도달하면 종료
             if new_position == 100:
-                return roll + 1
+                return roll+1
+            # 목적지보다 적은 경우만 갈 수 있음
+            elif new_position < 100:
 
-            if new_position < 100:
-                # 뱀 또는 사다리 위치로 이동
-                if my_map[new_position] != 0:
+                # 목적지에 순간이동 포탈이 있는 경우.
+                if my_map[new_position] != 0 and not visited[new_position]:
+                    visited[new_position] = True
                     new_position = my_map[new_position]
-
-                if not visited[new_position]:
                     visited[new_position] = True
                     queue.append((new_position, roll + 1))
 
-ladder, snake = map(int, input().split())
-my_map = [0] * 101
+                # 목적지가 일반 발판인 경우
+                elif my_map[new_position] == 0 and not visited[new_position]:
+                    # 새로운 포지션으로 도착했으므로 방문을 True로 설정
+                    visited[new_position] = True
+                    queue.append((new_position, roll + 1))
 
-for _ in range(ladder + snake):
-    start, end = map(int, input().split())
-    my_map[start] = end
 
-print(bfs(my_map))  # BFS를 호출하여 최단 경로를 찾음
+print(bfs())  # BFS를 호출하여 최단 경로를 찾음
