89b92edafca560ed4f7c7277b3c0afbbd38565e9
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-10-19 22:42:30 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,17 +1,14 @@
 a, b, c = map(int, input().split())
-
-# 반복적으로 (a^b) % c를 계산하는 함수
-def mod_pow(a, b, c):
-    result = 1
-    a = a % c
-    
-    while b > 0:
-        if b % 2 == 1:
-            result = (result * a) % c
-        a = (a * a) % c
-        b = b // 2
-    
-    return result
-
-result = mod_pow(a, b, c)
-print(result)
+# b는 제곱할 수가 된다.
+def pow(b):
+    if b == 0:
+        return 1
+    # 홀수
+    if b % 2 == 1:
+        h_pow = pow(b // 2)
+        return (a * h_pow * h_pow) % c
+    # 짝수
+    else:
+        h_pow = pow(b // 2)
+        return (h_pow * h_pow) % c
+print(pow(b))
