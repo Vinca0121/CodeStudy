2f8121c80d3e7fe1496753646a4709cc86401c0b
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-10-30 17:10:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1644 ms, Memory: 129456 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,23 +1,22 @@
 import sys
-
 input = sys.stdin.readline
+row, t = map(int, input().rstrip().split())
 
-row, t = map(int, input().split())
-my_map = [list(map(int, input().split())) for _ in range(row)]
+my_map = [list(map(int, input().rstrip().split())) for _ in range(row)]
+stack_sum = [[0 for _ in range(row+1)] for _ in range(row)]
 
-# 누적 합을 계산할 배열 초기화
-prefix_sum = [[0] * (row + 1) for _ in range(row)]
-
+# 누적합을 채운다.
 for i in range(row):
     for j in range(row):
-        prefix_sum[i][j + 1] = prefix_sum[i][j] + my_map[i][j]
-
+        stack_sum[i][j+1] = stack_sum[i][j] + my_map[i][j]
 for _ in range(t):
-    case = list(map(int, input().split()))
+    #      0     1        2   3
+    # 시작 row, col / 종료 row, col
+    case = list(map(int, input().rstrip().split()))
 
+    # 행 만큼 반복
     summery = 0
-    for i in range(case[0] - 1, case[2]):
-        # 누적 합을 활용하여 열의 합을 계산
-        summery += prefix_sum[i][case[3]] - prefix_sum[i][case[1] - 1]
-
+    for i in range(case[2]-case[0]+1):
+        #  열을 전부 더한다.
+        summery += stack_sum[case[0]-1+i][case[3]] - stack_sum[case[0]-1+i][case[1]-1]
     print(summery)
