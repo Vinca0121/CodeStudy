65011c3a07931eedce8f3b0d2e29f7a116b02034
vinca Kim <98202797+Vinca0121@users.noreply.github.com>
2023-12-12 17:17:58 +0900

    [Bronze V] Title: Rook, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.py
@@ -0,0 +1,16 @@
+def print_rook():
+    print("  ___  ___  ___")
+    print("  | |__| |__| |")
+    print("  |           |")
+    print("   \\_________/")
+    print("    \\_______/")
+    print("     |     |")
+    print("     |     |")
+    print("     |     |")
+    print("     |     |")
+    print("     |_____|")
+    print("  __/       \\__")
+    print(" /             \\")
+    print("/_______________\\")
+
+print_rook()
